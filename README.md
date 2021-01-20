e4c2054c5958fb1a9063282e2232c71d61e33234
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2021-01-20 14:51:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Template website
+
+Webpack and Babel for React
+
+## Quick start
+
+- Install dependencies
+
+  npm install
+
+- Run Server
+
+  npm run dev-server
+
+- Server run on http://localhost:8080/
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import reactDom from 'react-dom'
+// import './styles/style.css';
+import './styles/style.scss'
+
+const App = () => {
+  return <h1>Template website</h1>
+}
+
+reactDom.render(<App />, document.getElementById('root'))
